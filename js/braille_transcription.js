0a73bd358b777cfe4d98a94bd6b562338449bcd1
Alyssa <alawton1@my.hpu.edu>
2022-10-08 18:36:20 -1000

    attempt to email via html

--- js/braille_transcription.js
+++ js/braille_transcription.js
@@ -1,28 +1,83 @@
+window.onload=function() {
+    document.getElementById("upload-button").style.display='none';
+}
 window.addEventListener('load', loadEvent)
+
+var file_srcs = []
+
+var body = ""
 
 function loadEvent() {
     console.log("html loaded!")
-    $(document).ready(function () {
+
+    // if (localStorage.getItem("file_srcs") == null) {
+    //     console.log("Created file_srcs")
+    //     file_srcs = [];
+    //     localStorage.setItem("file_srcs", JSON.stringify(file_srcs));
+    // }
+    $(document).ready(function() {
         if (window.File && window.FileList && window.FileReader) {
-            $("#files").on("change", function (e) {
+            showUploadButton();
+
+            $("#files").on("change", function(e) {
                 var files = e.target.files,
                     filesLength = files.length;
+
                 for (var i = 0; i < filesLength; i++) {
                     var f = files[i]
-                    // console.log(f)
+                    var f_name = f.name
+
+                    console.log("file name: " + f_name)
+
+                    
+
+                    
+                    // body+= f;
+                    // body += "<img src=" + f_name + " alt=\"Unable to upload file\">"
+
+
+                    // var file_id = parseInt(localStorage.getItem("file_id")) + 1;
+
+                    // localStorage.setItem("file_id", file_id.toString())
 
                     if (f.type === "application/pdf") {
                         console.log("PDF!")
                     } else {
                         var fileReader = new FileReader();
-                        fileReader.onload = (function (e) {
+
+                        fileReader.onload = (function(e) {
                             var file = e.target;
 
-                            //previous
+                            // const newImage = new Image(100, 200);
+                            // newImage.src = file.result;
+
+                            // var canvas = document.createElement("canvas");
+                            // context = canvas.getContext('2d');
+                            
+                            // makeBase(file);
+
+                            // var pngUrl = canvas.toDataURL();
+
+                            // body += pngUrl;
+
+                            // body+=getBase64Image(newImage);
+
+                            // body += "<img src=" + f_name + " alt=\"Unable to upload file\">"
+
+                            // file_srcs = JSON.parse(localStorage.file_srcs);
+                            // file_srcs.push(file.result);
+                            // localStorage.setItem("file_srcs", JSON.stringify(file_srcs));
+
+                            // console.log("name" + f_name);
+
+                            // previous
                             // $("<span class=\"pip\">" +
-                            //     "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
+                            //     "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + f_name + "\"/>" +
                             //     "<br/><span class=\"remove\">Remove image</span>" +
                             //     "</span>").insertAfter("#files");
+
+                             // previous
+                            // $("<img src=\"" + e.target.result+ "\"/ >").insertAfter("#files");
 
                             // modified v-1
                             // $("<div class=\"card\">" +
@@ -33,23 +88,32 @@ function loadEvent() {
                             //     "<br/></div>").insertAfter("#files");
 
                             // modified v-2
-                            document.getElementById("photo-preview").innerHTML += "<div class=\"col mb-4\"> "+
-                            "<br/><div class=\"card\">" +
-                            "<br/><img class=\"card-img\" src=\"" + file.result + "\" alt=\"" + file.name + "\"/>" +
-                            "<br/><div class=\"card-img-overlay text-right\">"+
-                            "<br/><a class=\"close\" href=\"#\"><i class=\"fa fa-trash fa-lg\"></i></a>"+
-                            "<br/></div>" +
-                            "<br/></div>" +
-                            "<br/></div>";
+
+                            // document.getElementById("photo-preview").innerHTML += "<div class=\"col mb-4\"> " +
+                            //     "<br/><div class=\"card\">" +
+                            //     "<br/><img class=\"card-img\" src=\"" + file.result + "\" alt=\"" + f_name + "\"/>" +
+                            //     "<br/><div class=\"card-img-overlay text-right\" id=\"file_" + file_id + "\">" +
+                            //     "<br/><button class=\"close\" id=\"file_" + file_id + "\" href=\"#\"><i class=\"fa fa-trash fa-lg\"></i></button>" +
+                            //     "<br/></div>" +
+                            //     "<br/></div>" +
+                            //     "<br/></div>";
+
 
                             // Old code here
-                            /*$("<img></img>", {
-                              class: "imageThumb",
-                              src: e.target.result,
-                              title: file.name + " | Click to remove"
-                            }).insertAfter("#files").click(function(){$(this).remove();});*/
+                            $("<img></img>", {
+                                class: "card-img mx-2",
+                                src: file.result,
+                                title: file.name + " | Click to remove"
+                            }).insertAfter("#files").click(function() {
+                                // file_srcs = JSON.parse(localStorage.file_srcs);
+                                // console.log($(this));
+                                // file_srcs = removeItemOnce(file_srcs, $(this).src);
+                                $(this).remove();
+                            });
+
 
                         });
+
                         fileReader.readAsDataURL(f);
                     }
 
@@ -58,8 +122,66 @@ function loadEvent() {
         } else {
             alert("Your browser doesn't support to File API")
         }
+
+        // document.getElementById("upload-button").onclick = function () {
+        //     window.open('mailto:alawton1@my.hpu.edu?subject=Touch Tap LLC, Braille Transcription Request&body=' + encodeURIComponent(body));
+        // }
     });
 
+
+
+    // if (document.querySelector('.close') != null) {
+    //     console.log("HERE")
+    //     document.querySelector('.close').onclick(function() {
+    //         $(this).remove();
+    //     });
+    // }
+
+}
+
+function htmlToText(html) {
+    var temp = document.createElement('div');
+    temp.innerHTML = html;
+    return temp.textContent;
+}
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
+
+function showUploadButton() {
+    document.getElementById("upload-button").style.display='block';
+}
+
+function getBase64Image(img) {
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // Get the data-URL formatted image
+    // Firefox supports PNG and JPEG. You could check img.src to
+    // guess the original format, but be aware the using "image/jpg"
+    // will re-encode the image.
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+function makeBase(img) {
+    baseImage = new Image();
+    baseImage.src = img.result;
+    baseImage.onload = function() {
+        context.drawImage(baseImage, 100, 100);
+    }
 }
 
 /* PDF Coversion ---
