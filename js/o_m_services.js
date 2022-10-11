7f1600c30a873e46f71aad68eeb70e3c6ebfb41e
Alyssa <alawton1@my.hpu.edu>
2022-10-10 20:58:09 -1000

    o&m page summary
    popover order summary, stores id

--- js/o_m_services.js
+++ js/o_m_services.js
@@ -1,44 +1,115 @@
+// import updatePopover from './setup.js';
+import {Service, generateTable, getData, setData} from './setup.js';
+// import getData from './setup.js';
+// import setData from './setup.js';
+// import generateTable from './setup.js';
+
 window.addEventListener('load', loadEvent)
 
-function loadEvent(){
+function loadEvent() {
     console.log("html loaded!")
+
     window.onload = setupLocalStorage();
 
-    $("button").click(function() {
+    $("button").click(function () {
+        // console.log($('[data-toggle="popover"]'))
+        $('[data-toggle="popover"]').popover("hide");
         var html = document.getElementById(this.id).innerHTML;
-        var o_m_svcs_sel = JSON.parse(localStorage.getItem("o_m_svcs_sel"));
+        var o_m_svcs_sel = getData("o_m_svcs_sel");
+        // console.log("services" + o_m_svcs_sel)
         if (html === 'Select') {
             $(this).html('Selected');
-            if(!o_m_svcs_sel.includes(this.id)) o_m_svcs_sel.push(this.id);
+            var service = document.getElementById("h5-" + this.id).innerHTML;
+            var cost = document.getElementById("cost-" + this.id).innerHTML;
+
+            if (!containsObject(o_m_svcs_sel, this.id)) {
+                // $("#tbody-cart").append("<tr id=\"row-" + this.id + "\"> <td scrope=\"col\">" + service + "</td> <td scrope=\"col\">" + cost + "</td> </tr>");
+                o_m_svcs_sel.push(new Service(this.id, service, cost));
+            }
+            console.log("added:" + o_m_svcs_sel)
         } else {
             $(this).html('Select');
+            // console.log("remove:",this.id)
             removeVal(o_m_svcs_sel, this.id);
+            // $('table#cart-table tr#row-' + this.id).remove();
+            // if (o_m_svcs_sel.length == 0) $("#dark-row").remove();
+            console.log("removed:" + o_m_svcs_sel)
         }
-        localStorage.setItem("o_m_svcs_sel",JSON.stringify(o_m_svcs_sel));
-        console.log(localStorage.o_m_svcs_sel);
+
+        
+        // if (o_m_svcs_sel.length == 0) {
+        //     $("#sum-cart").text("$0");
+        // } else {
+        //     updateSum('cart-table', o_m_svcs_sel);
+        // }
+        setData("o_m_svcs_sel", o_m_svcs_sel);
+        generateTable('cart-table', o_m_svcs_sel);
+        console.log(o_m_svcs_sel);
     });
 
 }
 
-function removeVal(array, val) {
-    const index = array.indexOf(val);
-    if (index > -1) {
-        array.splice(index, 1);
+function removeVal(arr, val) {
+    // console.log("Array")
+    // console.log("index:" + getIndex(val, arr))
+    arr.splice(getIndex(val, arr), 1);
+}
+
+function getIndex(objId, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id === objId) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function setupLocalStorage() {
+    console.log("getdata:" , getData)
+    var o_m_svcs_sel = getData("o_m_svcs_sel");
+
+    console.log("services:" + o_m_svcs_sel)
+
+    if (o_m_svcs_sel === undefined) {
+        console.log("undefined")
+    } else {
+        if (o_m_svcs_sel === null) {
+            o_m_svcs_sel = [];
+            setData("o_m_svcs_sel", o_m_svcs_sel);
+        } else {
+            console.log("LEN:" + o_m_svcs_sel.length)
+            if (o_m_svcs_sel.length === 0) {
+                return;
+            }
+            o_m_svcs_sel.forEach(function (svc) {
+                let svcID = svc.id;
+                var html = document.getElementById(svcID).innerHTML;
+
+                // let service = document.getElementById("h5-" + svc).innerHTML;
+                // let cost = document.getElementById("cost-" + svc).innerHTML;
+                // console.log("HERE:" + $("#tbody-cart"))
+                // $("#tbody-cart").append("<tr id=\"row-" + svc + "\"> <td scrope=\"col\">" + service + "</td> <td scrope=\"col\">" + cost + "</td> </tr>");
+                if (html === 'Select') {
+                    document.getElementById(svcID).click();
+                    document.getElementById(svcID).innerHTML = 'Selected';
+                }
+            });
+            generateTable('cart-table', o_m_svcs_sel);
+            // updatePopover();
+        }
     }
 }
 
-function setupLocalStorage () {
-    if (localStorage.getItem("o_m_svcs_sel")===null) {
-        var o_m_svcs_sel = [];
-        localStorage.setItem("o_m_svcs_sel",JSON.stringify(o_m_svcs_sel));
-    } else {
-        var o_m_svcs_sel = JSON.parse(localStorage.getItem("o_m_svcs_sel"));
-        o_m_svcs_sel.forEach(elem_id => {
-            var html = document.getElementById(elem_id).innerHTML;
-            if (html === 'Select') {
-                document.getElementById(elem_id).click();
-                document.getElementById(elem_id).innerHTML = 'Selected';
-            }
-        });
+
+
+function containsObject(obj, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === obj) {
+            return true;
+        }
     }
+    return false;
 }
+
+
+
--- js/setup.js
+++ js/setup.js
@@ -3,27 +3,28 @@ function is_user_mode() {
 }
 
 // allow html pages to include html scripts in '/components'
-load_html_count = 0
-nav_bar_loaded_callbacks = []
-$(function() {
+var load_html_count = 0
+var nav_bar_loaded_callbacks = []
+$(function () {
     console.log("Including html files...")
     var includes = $('[data-include]')
-    $.each(includes, function() {
+    $.each(includes, function () {
         var base_file_name = $(this).data('include');
         var file = 'components/' + base_file_name + '.html'
 
         load_html_count += 1
-        $(this).load(file, function() {
+        $(this).load(file, function () {
             load_html_count -= 1
 
             if (load_html_count == 0) {
                 console.log("HTML File Loaded")
 
+                // $('#purchaseModal').modal({backdrop: 'static', keyboard: false});
                 console.log("Setting active item in menu")
                 var url = window.location.href;
 
                 // passes on every "a" tag
-                $("#h_menu a").each(function() {
+                $("#h_menu a").each(function () {
                     // checks if its the same on the address bar
                     if (url == (this.href)) {
                         $(this).closest("li").addClass("active");
@@ -31,39 +32,139 @@ $(function() {
                 });
 
                 // passes on every "a" tag
-                $("#v_menu a").each(function() {
+                $("#v_menu a").each(function () {
                     // checks if its the same on the address bar
                     if (url == (this.href)) {
                         $(this).closest("li").addClass("active");
                     }
                 });
 
-                if ($('[data-toggle="popover"]').length > 0) {
-                    $('[data-toggle="popover"]').popover({
-                        html: true,
-                        sanitize: false,
-                        placement: 'bottom',
-                        title: function() {
-                            return $("#popover-head").html();
-                        },
-                        content: function() {
-                            console.log("Pop over CONTENT in SETUP")
-                            return $("#popover-content").html();
-                        }
-                    }).on('shown.bs.popover', function() {
-
-                        // Trying to get date picker to show :(
-                        // $('#reservationDate').datepicker({
-                        //     format: "dd/mm/yyyy"
-                        // }).on('change', function() {
-                        //     $('.reservationDate').hide();
-                        // });
-                    });
+                var o_m_svcs_sel = getData("o_m_svcs_sel");
+                if (o_m_svcs_sel === null) {
+                    o_m_svcs_sel = [];
+                    setData("o_m_svcs_sel", o_m_svcs_sel);
                 }
+
+                generateTable('cart-table', o_m_svcs_sel);
+
+                updatePopover();
+
+                $('#checkout_btn').attr('name', 'value');
+
+                console.log("button", $("button#checkout"))
+
+
             }
 
         })
 
     })
-    
+
 })
+
+$(document).on('click', '#checkout_btn', function () {
+    $('[data-toggle="popover"]').popover("hide");
+    $('#tbody-cart').empty();
+    setData("o_m_svcs_sel", []);
+    updateSum([]);
+    $('#purchaseModal').modal({backdrop: 'static', keyboard: false});
+    $('#purchaseModal').on('shown.bs.modal', function () {
+        $('#purchaseModal').focus();
+    })  
+    // $('#purchaseModal').trigger("focus")
+    // window.location.reload();
+});
+
+$(document).on('click', '#modal_close', function () {
+    window.location.reload();
+});
+
+function updatePopover() {
+    if ($('[data-toggle="popover"]').length > 0) {
+        $('[data-toggle="popover"]').popover({
+            html: true,
+            sanitize: false,
+            placement: 'bottom',
+            title: function () {
+                return $("#popover-head").html();
+            },
+            content: function () {
+                console.log("Pop over CONTENT in SETUP")
+                return $("#popover-content").html();
+            }
+        }).on('shown.bs.popover', function () {
+
+            // Trying to get date picker to show :(
+            // $('#reservationDate').datepicker({
+            //     format: "dd/mm/yyyy"
+            // }).on('change', function() {
+            //     $('.reservationDate').hide();
+            // });
+        });
+    }
+}
+
+export class Service {
+    constructor(id, name, cost) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+export function generateTable(tableId, o_m_svcs_sel) {
+    o_m_svcs_sel = getData("o_m_svcs_sel");
+    let o_m_svcs_ids = []
+    // console.log("services:" + o_m_svcs_sel)
+
+    if (o_m_svcs_sel === null) {
+        o_m_svcs_sel = [];
+        setData("o_m_svcs_sel", o_m_svcs_sel);
+    } else {
+        $('#tbody-cart').empty()
+        o_m_svcs_sel.forEach(svc => {
+            let id = svc.id;
+            // console.log("HERE:" + $("#tbody-cart"))
+            $("#tbody-cart").append("<tr id=\"row-" + id + "\"> <td scrope=\"col\">" + svc.name + "</td> <td scrope=\"col\">" + svc.cost + "</td> </tr>");
+            o_m_svcs_ids.push(id);
+        });
+        updateSum(o_m_svcs_sel);
+        if (o_m_svcs_sel.length == 0) {
+            $('button#checkout_btn').prop('disabled', true);
+        } else {
+            $('button#checkout_btn').prop('disabled', false);
+        }
+    }
+    // console.log("ids",o_m_svcs_ids)
+    $('input#checkout').val(o_m_svcs_ids);
+}
+
+export function updateSum(o_m_svcs_sel) {
+    let sum = 0;
+
+    o_m_svcs_sel.forEach(svc => {
+        sum += parseInt((svc.cost).replace("$", ""));
+        // console.log(sum)
+    });
+
+    // for (let index = 2; index < o_m_svcs_sel.length + 2; index++) {
+    //     console.log("i:" + index)
+    //     // console.log("CELL:" + table.rows[index].cells[1].innerHTML.replace("$",""));
+    //     sum += parseInt(table.rows[index].cells[1].innerHTML.replace("$", ""));
+    //     // console.log("SUM=" + sum)
+    // }
+    // // console.log("cell:" + document.getElementById('cart-table').rows[1].cells[0].innerHTML)
+    // console.log("sum" + sum)
+    $("#sum-cart").text("$" + sum);
+}
+
+export function getData(key) {
+    // console.log("localStorage:", localStorage)
+    const data = localStorage.getItem(key);
+    if (data === null) return null;
+    return JSON.parse(data);
+}
+
+export function setData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
